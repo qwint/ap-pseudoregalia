55a367a41888b2ed6b08d9ecf29ec5de991de06f
qwint <qwint.42@gmail.com>
2024-11-24 21:34:21 -0600

    initial option docstrings from merikatt

--- options.py
+++ options.py
@@ -77,6 +77,13 @@ class ShuffleChairs(Toggle):
 
 
 class UpstreamTrick(Choice):
+    """
+    Disabled means doesnt expect anything, its just there as a placeholder so you can select something othere than an actual difficutly
+    Normal means you can do it somewhat easily after just being told about it essentialy first 3-5 tries with no prior experience with it
+    Advanced means its not new to you, the application has easy retries or isnt neccassairly hard, would need 10 or more tries if you had no previous experience in the trick category
+    Expert means you know what youre doing with the trick and the application of it might be harder to do or harder to retry/easier to die attempting it
+    Insane is a  catch all for everything above that and essentially means that the trick is second nature to you and you can do expert and below more or less first try with little to no difficulty
+    """
     option_disabled = 0
     option_normal = 1
     option_advanced = 2
@@ -86,39 +93,52 @@ class UpstreamTrick(Choice):
 
 
 class TrickClingAbuse(UpstreamTrick):
-    """Abuse of cling to climb corners"""
+    __doc__ = UpstreamTrick.__doc__ + """
+    ClingAbuse is climbing corners with cling"""
     display_name = "Cling Abuse"
 
 
 class TrickKnowledge(UpstreamTrick):
-    """Things like enabling levers/breaking walls with sunsetter"""
+    __doc__ = UpstreamTrick.__doc__ + """
+    Knowledge is things like activating levers /breaking walls with sunsetter, essentially anything that isnt obvious that you can do but that you can do."""
     display_name = "Knowledge"
 
 
 class TrickMovement(UpstreamTrick):
-    """General movement such as backflips"""
+    __doc__ = UpstreamTrick.__doc__ + """
+    Movement is general movement such as standing backflips or jumping around a tight corner"""
     display_name = "Movement"
 
 
 class TrickMomentum(UpstreamTrick):
-    """Momentum conservation"""
+    __doc__ = UpstreamTrick.__doc__ + """
+    Momentum conservation is things like using cling and jumping of a pillar to get a backflip (see empty bailey bridge to ruins)"""
     display_name = "Momentum"
 
 
 class TrickOneWall(UpstreamTrick):
-    """Single Wall wall-kicks"""
+    __doc__ = UpstreamTrick.__doc__ + """
+    OneWall is just wall kicking up a single wall without a corner or another wall"""
     display_name = "One Wall"
 
 
 class TrickReverseKick(UpstreamTrick):
-    """Reverse wall kicks"""
+    __doc__ = UpstreamTrick.__doc__ + """
+    ReverseKick is a reverse wall kick"""
     display_name = "Reverse Kick"
 
 
 class TrickSunsetterAbuse(UpstreamTrick):
-    """Abuse the backflip"""
+    __doc__ = UpstreamTrick.__doc__ + """
+    Sunsetter abuse is abusing the sunsetter backflip since it can be kinda floaty (see the bridge again, you can cross it with only sunsetter)"""
     display_name = "SunsetterAbuse"
 
+class TrickPogoAbuse(UpstreamTrick):
+    # currently unused?
+    __doc__ = UpstreamTrick.__doc__ + """
+    PogoAbuse is using ascendent light in ways vanilla doesnt "expect"
+    """
+    display_name = "PogoAbuse"
 
 @dataclass
 class pseudoregalia_options(PerGameCommonOptions):
